a3951782a0194463df7704901205a4339723290c
Jose David Caro <j.caro0207@gmail.com>
2021-08-05 17:09:17 -0500

    Add task 4

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,33 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require auth public method"""
-        return False
+
+        if path is None or excluded_paths is None or excluded_paths = []:
+            return True
+
+        lenPath = len(path)
+        if lenPath == 0:
+            return True
+
+        slash_path = True if path[lenPath - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorized header public method"""
