776e86adccb730ca289c2a7a368a2033d07489b0
Jose David Caro <j.caro0207@gmail.com>
2021-08-05 16:56:18 -0500

    Add task 3

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+from flask import request
+from typing import List, TypeVar
+""" Auth file"""
+
+
+class Auth():
+    """ class auth from authentication file"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require auth public method"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorized header public method"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
