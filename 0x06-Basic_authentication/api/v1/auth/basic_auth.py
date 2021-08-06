ca3281d02e2fd002dcb18a6732e7d3d15507f3a7
Jose David Caro <j.caro0207@gmail.com>
2021-08-05 19:06:36 -0500

    Add task 8

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
 """ basic authentication for auth"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
     """ Basic authentication class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """ Extract base64 authorization header"""
-        if authorization_header is None or  not isinstance(authorization_header, str):
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
             return None
 
         if not authorization_header.startswith('Basic '):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ decode authorization header"""
+
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            encoded = base64_authorization_header.encode('utf-8')
+            decoded64 = b64decode(encoded)
+            return decoded64.decode('utf-8')
+        except BaseException:
+            return None
