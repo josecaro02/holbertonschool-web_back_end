2e64dc784a3c72ead4df5addfd8a2afd5bbf6fbe
Jose David Caro <j.caro0207@gmail.com>
2021-08-11 18:14:29 -0500

    Add task 6

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ basic session for auth"""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -27,3 +28,13 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ overload that returns user insacens based on cookie"""
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        return User.get(user_id)
