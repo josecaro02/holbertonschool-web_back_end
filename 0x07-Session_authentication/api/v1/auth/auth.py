4c47d6423c0e2ddffb8e23a8a6cd07c6a12999a2
Jose David Caro <j.caro0207@gmail.com>
2021-08-11 16:29:36 -0500

    add task 0

---
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" Auth file for authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ class auth from authentication file"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require auth public method"""
+
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        lenPath = len(path)
+        if lenPath == 0:
+            return True
+
+        slash_path = True if path[lenPath - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ authorized header public method"""
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user public method"""
+        return None
