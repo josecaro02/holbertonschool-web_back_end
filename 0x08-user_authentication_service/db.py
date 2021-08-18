566709404a0ea82be60440b5432ef7af97db21c8
Jose David Caro <j.caro0207@gmail.com>
2021-08-18 17:58:28 -0500

    Echo false in engine

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module for project user authentication
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
 from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class for project user authentication
     """
 
     def __init__(self) -> None:
@@ -32,17 +33,16 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Method that save theuser to the database
-        return a User object"""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        """ method, which has two required string arguments:
+         email and hashed_password, and returns a User object"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
+
+        return user
 
     def find_user_by(self, **kwargs) -> User:
-        """ Finds user by aney keyword arguments
-        Return: First row found in the users table
-        """
+        """find user by method return first row founded"""
         if not kwargs:
             raise InvalidRequestError
 
@@ -50,8 +50,10 @@ class DB:
         for key in kwargs.keys():
             if key not in column_names:
                 raise InvalidRequestError
-        found_user = self._session.query(User).filter_by(**kwargs).first()
-        if found_user is None:
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
             raise NoResultFound
 
-        return found_user
+        return user
