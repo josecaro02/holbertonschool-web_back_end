562f5a21681b2f06c339fe556e068ade294930d3
Jose David Caro <j.caro0207@gmail.com>
2021-08-18 22:03:31 -0500

    [Add task 14]

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ basic flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import (Flask, jsonify, request, abort, redirect)
 from auth import Auth
 
 
@@ -48,6 +48,27 @@ def login():
         return output
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def log_out() -> str:
+    """Find the user with the requested session ID.
+    If the user exists destroy the session and redirect the user to GET /.
+    If the user does not exist, respond with a 403 HTTP status.
+    """
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
