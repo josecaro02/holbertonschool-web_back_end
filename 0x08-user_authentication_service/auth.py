441f7e4560b26c715d469939970b0952d56a76ba
Jose David Caro <j.caro0207@gmail.com>
2021-08-18 21:58:13 -0500

    Add task 11

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 import uuid
+from typing import Union
 
 
 def _hash_password(password: str) -> str:
@@ -67,3 +68,16 @@ class Auth:
             new_uuid = _generate_uuid()
             self._db.update_user(found_user.id, session_id=new_uuid)
             return new_uuid
+
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """It takes a single session_id string argument and
+        returns the corresponding User or None."""
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
