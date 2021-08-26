3f5fa8236146197f261954d153b84b7d80f38bcf
Jose David Caro <j.caro0207@gmail.com>
2021-08-26 17:47:40 -0500

    add task 1

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+''' Flask base module'''
+
+from flask import Flask
+import flask
+from flask_babel import Babel
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config(object):
+    # ...
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """ return 0-index template"""
+    return flask.render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
