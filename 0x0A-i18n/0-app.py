f96d520d658754caa3f7241ad8c1246a50c63108
Jose David Caro <j.caro0207@gmail.com>
2021-08-26 17:30:23 -0500

    Reverse html code and add template folder

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -4,13 +4,13 @@
 from flask import Flask
 import flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def hello_world() -> str:
     """ return 0-index template"""
-    return flask.render_template('templates/0-index.html')
+    return flask.render_template('0-index.html')
 
 
 if __name__ == "__main__":
