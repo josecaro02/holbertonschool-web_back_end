45b2adba5c4b88eb4b8ac847b75e5106f33f7516
Jose David Caro <j.caro0207@gmail.com>
2021-08-31 23:41:29 -0500

    Change filename

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+''' Excercise file from redis basic'''
+import redis
+from typing import Union
+from uuid import uuid4
+
+
+class Cache:
+    ''' Cache class for redis'''
+
+    def __init__(self):
+        ''' init method cache class'''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        ''' store method to save data in redis'''
+        keyData = str(uuid4())
+        self._redis.set(keyData, data)
+        return(keyData)
