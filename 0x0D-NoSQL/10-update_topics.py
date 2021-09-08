ad5ee8c569d1b601454578f248c28e143fe3482c
Jose David Caro <j.caro0207@gmail.com>
2021-09-07 19:23:07 -0500

    add task 10

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' changes all topics of a school document based on the name '''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''changes all topics of a school document based on the name'''
+    filter = {'name': name}
+    newvalues = {"$set": {'topics': topics}}
+    mongo_collection.update(filter, newvalues)
