40867447f02c1b0271753e8b2c7b5c0a4adda314
Jose David Caro Cantor <j.caro0207@gmail.com>
2021-10-03 23:54:04 -0500

    Fix 5 test file

--- 0x13-unittests_in_js/5-payment.test.js
+++ 0x13-unittests_in_js/5-payment.test.js
@@ -1,8 +1,32 @@
+const sinon = require('sinon');
+var { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./5-payment');
 const Utils = require('./utils');
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${result}`);
-}
+describe('Hooks', function () {
+  let spyConsole;
+  beforeEach(function () {
+    // runs before each test in this block
+    spyConsole = sinon.spy(console, 'log');
+  });
 
-module.exports = sendPaymentRequestToApi;
+  afterEach(function () {
+    // runs after each test in this block
+    spyConsole.restore();
+  });
+
+  it('logs correctly with 100, 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(spyConsole.calledOnce).to.be.true;
+  });
+
+  it('logs correctly with 10, 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(spyConsole.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(spyConsole.calledOnce).to.be.true;
+  });
+});
